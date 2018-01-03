8c2fc2e9d29d6e8d106a5c9b29b8d1dcd6ba2fc3
Bryce Cordill <brycecordill@gmail.com>
2018-01-03 09:26:24 -0800

    Add button for spinning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Relic/TeleOp_3650.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Relic/TeleOp_3650.java
@@ -12,9 +12,15 @@ public class TeleOp_3650 extends OpMode{
     private DcMotor lDrive, rDrive, lift1;
     private Servo armServo, grabber;
 
+    boolean motorBusy = false;
+    IMU_class imu;
+    double initialHeading;
+
 
     @Override
     public void init() {
+        imu = new IMU_class("imu", hardwareMap);
+        initialHeading = getHeading(imu);
         rDrive = hardwareMap.dcMotor.get("rDrive");
 
         lDrive = hardwareMap.dcMotor.get("lDrive");
@@ -40,28 +46,36 @@ public class TeleOp_3650 extends OpMode{
             lift1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
 
-        if(gamepad1.a){
-            lDrive.setPower(.6);
-            rDrive.setPower(.6);
+        if (gamepad1.x){ // Spin 180ish degrees with one button
+            motorBusy = true;
+            //MIGHT need to thread this
+            turn2Angle(-179, imu);
         }
-        else if (gamepad1.y){
-            lDrive.setPower(-.6);
-            rDrive.setPower(-.6);
+        else if(!lDrive.isBusy()){
+            motorBusy = false;
         }
-        else if (gamepad1.b){  // full forward
-            lDrive.setPower(-.99);
-            rDrive.setPower(-.99);
-        }
-        else{
-            lDrive.setPower(gamepad1.left_stick_y*.6);
-            rDrive.setPower(gamepad1.right_stick_y*.6);
+
+        if (!motorBusy) {
+            if (gamepad1.a) {
+                lDrive.setPower(.6);
+                rDrive.setPower(.6);
+            } else if (gamepad1.y) {
+                lDrive.setPower(-.6);
+                rDrive.setPower(-.6);
+            } else if (gamepad1.b) {  // full forward
+                lDrive.setPower(-.99);
+                rDrive.setPower(-.99);
+            } else {
+                lDrive.setPower(gamepad1.left_stick_y * .6);
+                rDrive.setPower(gamepad1.right_stick_y * .6);
+            }
         }
 
         // lift
-        if (gamepad2.right_trigger > 0.1){ //&& lift1.getCurrentPosition() < 30004){
+        if (gamepad2.right_trigger > 0.1){
             lift1.setPower(gamepad2.right_trigger);
         }
-        else if (gamepad2.left_trigger > 0.1){ //&& lift1.getCurrentPosition() >= 0){
+        else if (gamepad2.left_trigger > 0.1){
             lift1.setPower(-gamepad2.left_trigger);
         }
 
@@ -95,4 +109,39 @@ public class TeleOp_3650 extends OpMode{
         telemetry.addData("Servo value: ", armServo.getPosition());
 
     }
+    void turn2Angle(double target, IMU_class i){
+        lDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        initialHeading = getHeading(i);
+        double relTarget = initialHeading + target;
+        if (0 > target){
+            //Turn right
+            lDrive.setPower(.7);
+            rDrive.setPower(-.7);
+            while (Math.abs(relTarget - getHeading(i)) > 5){
+                telemetry.addData("degrees to target", Math.abs(getHeading(i) - relTarget));
+                telemetry.addData("current heading", getHeading(i));
+                telemetry.update();
+            }
+        }
+        else{
+            //Turn left
+            lDrive.setPower(-.7);
+            rDrive.setPower(.7);
+            while (Math.abs(relTarget - getHeading(i)) > 5){
+                telemetry.addData("degrees to target", Math.abs(getHeading(i) - relTarget));
+                telemetry.addData("current heading", getHeading(i));
+                telemetry.update();
+            }
+        }
+        setBothPower(0.0);
+    }
+    double getHeading(IMU_class a){
+        return a.getAngles()[0];
+    }
+    void setBothPower(double power){
+        lDrive.setPower(power);
+        rDrive.setPower(power);
+    }
+
 }
